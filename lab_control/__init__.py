fbfe05f949662490044c73a2610388717abb3af9
Keisuke Fujii <fujiisoup@gmail.com>
2020-10-13 13:20:51 +0900

    Added a readme

--- lab_control/__init__.py
+++ lab_control/__init__.py
@@ -1,3 +1,14 @@
-from . import wavemeter
-from .usb_1208LS import usb_1208LS
-from .pwr01 import pwr01
+try:
+    from . import wavemeter
+except ImportError:
+    pass
+
+try:
+    from .usb_1208LS import usb_1208LS
+except ImportError:
+    pass
+
+try:
+    from .pwr01 import pwr01
+except ImportError:
+    pass
