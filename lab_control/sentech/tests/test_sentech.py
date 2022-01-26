27b7eee50fba94cf1ddd765c042ccf85a7f6739a
Keisuke Fujii <fujiisoup@gmail.com>
2022-01-26 11:58:09 +0900

    Added some more tests

--- lab_control/sentech/tests/test_sentech.py
+++ lab_control/sentech/tests/test_sentech.py
@@ -1,4 +1,4 @@
-from multiprocessing.sharedctypes import Value
+import numpy as np
 from lab_control.sentech import SentechCamera
 
 
@@ -19,11 +19,18 @@ def test_get_configuration():
 
 def test_shoot_xr():
     camera = SentechCamera(0)
-    camera.configure(BinningVertical=1)
 
     images = camera.shoot(3, 0.1, return_xr=True)
-    print(images)
     camera.__del__()
 
+def test_shoot_exposure():
+    camera = SentechCamera(0)
+    images = camera.shoot(3, 0.1, return_xr=True)
+    assert np.allclose(np.diff(images['time'].values), 0.1, rtol=1e-4)
+
+    images = camera.shoot(3, 0.3, return_xr=True)
+    assert np.allclose(np.diff(images['time'].values), 0.3, rtol=1e-4)
+    
+    camera.__del__()
 
 
